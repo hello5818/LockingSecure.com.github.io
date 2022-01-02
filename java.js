89ab0b8fd85789ed461299c1bc2db9294bd391e6
hello5818 <84682148+hello5818@users.noreply.github.com>
2022-01-02 20:55:50 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,16 @@
+function slide()
+{
+var counter = 1;
+setInterval(function(){
+document.getElementById('radio' + counter).checked = true;
+counter++;
+if (counter >4) {
+counter=1;
+}
+},5000);
+}
+function lockingSecureDownloadalert()
+{
+alert('⚠️ Le téléchargement va commencer ! ⚠️');
+window.location.href = 'apk/LockingSecure.apk';
+}
