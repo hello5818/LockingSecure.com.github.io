ea8182b17acca410cca107ed8ad6a0eea004ee67
hello5818 <84682148+hello5818@users.noreply.github.com>
2022-01-03 08:51:57 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+
+<html lang="fr-FR">
+<html>
+<meta charset="UTF-8">
+ <link rel="stylesheet" href="LS.css"/>
+ <link rel="stylesheet" href="Download.css"/>
+ <script src="java.js" > </script>
+ <link rel="icon" type="Image/png" href="Image/LS.png">
+ <!-- Police d'écriture google-->
+ <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Play:wght@700&display=swap" rel="stylesheet">
+ <!-- Fin Police d'écriture google-->
+  
+<head>
+<title> Formulaire</title>
+ <!-- LiveChat-->
+<script>
+    (function(d, w, c) {
+        w.ChatraID = '5RScxfjZFWTxBzPac';
+        var s = d.createElement('script');
+        w[c] = w[c] || function() {
+            (w[c].q = w[c].q || []).push(arguments);
+        };
+        s.async = true;
+        s.src = 'https://call.chatra.io/chatra.js';
+        if (d.head) d.head.appendChild(s);
+    })(document, window, 'Chatra');
+</script>
+ <!-- Fin LiveChat-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+ </head>
+ 
+
+ 
+    <body>
+<!-- Menu, Header-->
+<ul>
+<div id="logo"><a href="index.html" > <img src="Image/logoLS.png" width="230"; height="90" > </a></div>
+ <li><a href="index.html" >Accueil 
+ <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+  <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/></svg></a></li>
+ 
+ <li><a href="Download.html" >Télécharger
+ <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-down-square-fill" viewBox="0 0 16 16">
+  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z"/></svg></a></li>
+ 
+ 
+  <li><a href="shop.html" > Magasin
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bag-fill" viewBox="0 0 16 16">
+  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"/></svg></a> </li>
+  
+ 
+ <li><a href="Formulaire.php" > Connexion
+ <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/></svg></a> </li>
+ </ul>
+<!-- FinMenu, Header-->
+
+         <div class="container">
+             <h1>Formulaires</h1>
+			 <p> Cette page recontre actuellement un problème à cause d'un problème lié à JavaScript Object Notation </p>
+              <form action="contactverif.php" method="POST">
+               <fieldset>
+                 <legend>Formulaire de contact</legend>
+                 
+                 <div class="form-group">
+                   <label for="nom">Entrez votre nom</label>
+                   <input type="nom" class="form-control" id="nom" >
+                 </div>
+                 
+                 <div class="form-group">
+                   <label for="email">Entrez votre mail</label>
+                   <input type="email" class="form-control" id="email" >
+                
+				</div>
+				   <div class="form-group">
+					<label for="message">Votre Message</label>
+					<textarea name="message" class="form-control" id="message" rows="10"></textarea>
+					<input type="submit">
+					</form>
+				</div>
+				
+
+ 
+
+  
+
+<footer>
+<p><div class="centrage"> | <a href="https://chatra.com/fr/"> Assistance </a> | <a href="index.php"> Contact </a>| <a href="MentionsLegales.html"> Mentions légales</a> |</p></div>
+<div class="trait"></div>
+<p> Copyright © 2021-2022 Locking Secure. Tous droits réservés.</p>
+
+</footer>
+
+
+
+ </body>
+  </html>
+
